aae238b82969c72c5596aea65cd746469634c2d1
STTR13 <simon.taeter@gmail.com>
2020-03-09 11:57:15 +0100

    dl torrent file + bdecode

--- video/torrent.js
+++ video/torrent.js
@@ -1,15 +1,39 @@
 /*jshint esversion: 6 */
 
-var die = function() {
+var die = function(msg) {
+  console.log(msg);
   process.kill(process.pid);
 };
 
-if (process.argv.length != 4) die();
+var readTorrent = function(fileName) {
+  const fileContent = fs.readFileSync(fileName);
+  const torrent = bencode.decode(fileContent);
 
+  return torrent;
+};
+
+// die if wrong amout of param
+if (process.argv.length != 4) die("Invalid param amount.");
+
+// requires
 const https = require('https');
 const fs = require('fs');
+const bencode = require('bencode');
 
-const file = fs.createWriteStream(process.argv[2] + ".torrent");
-const request = https.get(process.argv[3], function(response) {
-  response.pipe(file);
+// torent file name
+var fileName = process.argv[2] + ".torrent";
+
+// dl file if not exist
+fs.access(fileName, fs.constants.F_OK, (err) => {
+  if (err) {
+    const wfile = fs.createWriteStream(fileName);
+    const request = https.get(process.argv[3], (response) => {
+      response.pipe(wfile);
+    });
+  }
+  else {
+    torrent = readTorrent(fileName);
+    console.log(torrent.announce.toString('utf8'));
+  }
+
 });
