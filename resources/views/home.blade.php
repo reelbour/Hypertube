97d70d3d13b0555ef2498356b3871f896bcbc4d4
lillmike <vgula@student.s19.be>
2020-03-09 19:56:07 +0100

    Suggested movies showed correctly.
    TODO: make titles as a link to the movie page.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('text.dashboard')}}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-md-4">
+                        <div class="card mb-4 shadow-sm">
+                            <img src="{{$movie->medium_cover_image}}" alt="Movie cover">
+                            <div class="card-body">
+                                <p class="card-text">{{$movie->title}}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-muted">{{$movie->year}}</small>
+                                    <small class="text-muted">{{$movie->rating}}</small>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('text.log_txt')}}
-
-                    <ul>
-                        @foreach ($movies as $movie)
-                            <li>{{$movie->title}}</li>
-                            <li>{{$movie->year}}</li>
-                            <li>{{$movie->rating}}</li>
-                            <li>{{$movie->medium_cover_image}}</li>
-                            <hr>
-                        @endforeach
-                    </ul>
-                </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
