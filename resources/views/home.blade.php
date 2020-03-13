40ac11c56b3c76f3c9afb4027003bf0edd5c2e2d
lillmike <vgula@student.s19.be>
2020-03-13 11:04:02 +0100

    Sort in progress.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,73 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-
 <form class="" action="{{url('home/search')}}" method="get" style="text-align: center;">
-
   <div class="field">
-      <label class="label">{{ __('text.query')}}</label>
-      <div class="control">
-        <input type="text" name="query">
-      </div>
+    <label class="label">{{ __('text.query')}}</label>
+    <div class="control">
+        <input type="text" name="query" value="{{ isset($query) ? $query : '' }}">
+
+        <select class="custom-select" name="sort">
+          <option selected>Sort by</option>
+          <option name="nameasc" value="nameasc">Name (asc)</option>
+          <option name="namedesc" value="namedesc">Name (desc)</option>
+          <option name="yearasc" value="yearasc">Year (asc)</option>
+          <option name="yeardesc" value="yeardesc">Year (desc)</option>
+          <option name="imdbasc" value="imdbasc">IMDb note (asc)</option>
+          <option name="imdbdesc" value="imdbdesc">IMDb note (desc)</option>
+        </select>
+
+        <button type="submit" class="btn btn-primary">Search</button>
+    </div>
   </div>
-
-  <!-- <form  action="#" method="post">
-    <label for="genre">Choose a gender:</label>
-    <select id="genre" name="carlist" form="carform">
-      <option value=""></option>
-      <option value="Action">Action</option>
-      <option value="Aventure">Aventure</option>
-      <option value="Comedy">Comedy</option>
-      <option value="Humour">Humour</option>
-      <option value="Horror">Horror</option>
-
-    </select>
-    <br>
-    <label for="popularity">Choose between a car:</label>
-    <select id="popularity" name="carlist" form="carform">
-      <option value=""></option>
-      <option value="volvo">Volvo</option>
-      <option value="saab">Saab</option>
-      <option value="opel">Opel</option>
-      <option value="audi">Audi</option>
-    </select>
-  </form>
-
-  <div class="field">
-      <div class="control">
-        <button class="button is-link">{{ __('text.send')}}</button>
-      </div>
 </form>
-
-
-<form>
-  <label for="vol">popularity (between 0 and 10):</label>
-  <input type="range" id="vol" name="vol" min="0.0" max="10">
-  <label for="vol">popularity (between 0 and 10):</label>
-  <input type="range" id="vol" name="vol" min="0.0" max="10">
-</form>
-
-<form>
-  <label for="datemax">Enter a date before 1980-01-01:</label>
-  <input type="date" id="datemax" name="datemax" max="1979"><br><br>
-
-  <label for="datemin">Enter a date after 2000-01-01:</label>
-  <input type="date" id="datemin" name="datemin" min="2000-01-02"><br><br>
-
-  <label for="quantity">Quantity (between 1 and 5):</label>
-  <input type="number" id="quantity" name="quantity" min="1" max="5">
-
-  <label for="datemax">Enter a date before 1980-01-01:</label>
-  <input type="number" step="0.1" value="0" min="0" max="10">
-
-</form>>
-
-<br> <br> <br> <br>
-
-<form action="#" method="post">
-  <label for="number">Enter a popularity s:</label>
-  <input type="number" step="0.1" value="0" min="0" max="10">
-</form> -->
 
 <div class="container">
     <div class="row justify-content-center">
